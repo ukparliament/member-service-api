bce2c652ce92c76452b0467fab59e8ede2cbdbfb
Rebecca Appleyard <rklappleyard@gmail.com>
2016-11-10 17:40:22 +0000

    Added route for current members

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'index', to: 'application#index', as: :index
-
-  resources :people, only: [:index]
-
   get '/people/members', to: 'members#index'
+  get '/people/members/current', to: 'members#current'
+
+  resources :people, only: [:index, :show]
 
 end
