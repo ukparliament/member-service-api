e9af9eea90491761197aec0bbd5489ff4302c960
giusepped <giusdesan@gmail.com>
2016-11-11 11:08:04 +0000

    added route for parties

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   resources :people, only: [:index, :show]
 
   resources :contact_points, only: [:index, :show]
+
+  resources :parties, only: [:index, :show]
 end
