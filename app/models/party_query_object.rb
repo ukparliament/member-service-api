2c369d15a95e910e71a5224442a1cab951d88b62
Rebecca Appleyard <rklappleyard@gmail.com>
2016-11-22 11:04:30 +0000

    Added dates to members query for parties

--- app/models/party_query_object.rb
+++ app/models/party_query_object.rb
@@ -37,7 +37,6 @@ class PartyQueryObject
               CONSTRUCT {
                  ?party parl:partyName ?partyName .
               }
-
               WHERE {
                   ?party parl:partyName ?partyName .
                   FILTER (?party = <#{DATA_URI_PREFIX}/#{id}> )
@@ -62,12 +61,13 @@ class PartyQueryObject
                 WHERE {
                     ?party parl:partyHasPartyMembership ?partyMembership .
                     ?partyMembership parl:partyMembershipHasPerson ?member .
+                    OPTIONAL { ?partyMembership parl:partyMembershipStartDate ?partyMembershipStartDate . }
+        			      OPTIONAL { ?partyMembership parl:partyMembershipEndDate ?partyMembershipEndDate . }
                     ?member
                           a parl:Member .
-                    ?member parl:personHasSitting ?sitting .
-                            OPTIONAL { ?member parl:forename ?forename } .
-                            OPTIONAL { ?member parl:middleName ?middleName } .
-                            OPTIONAL { ?member parl:surname ?surname } .
+                          OPTIONAL { ?member parl:forename ?forename } .
+                          OPTIONAL { ?member parl:middleName ?middleName } .
+                          OPTIONAL { ?member parl:surname ?surname } .
                     FILTER (?party = <#{DATA_URI_PREFIX}/#{id}> )
                 }
                ")
@@ -81,6 +81,10 @@ class PartyQueryObject
                            parl:forename ?forename ;
                            parl:middleName ?middleName ;
                            parl:surname ?surname ;
+                   ?partyMembership a parl:PartyMembership ;
+                           parl:partyMembershipStartDate ?partyMembershipStartDate ;
+                           parl:partyMembershipEndDate ?partyMembershipEndDate ;
+                           parl:partyMembershipHasParty ?member .
                 }
 
                 WHERE {
