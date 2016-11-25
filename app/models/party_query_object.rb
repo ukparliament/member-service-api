ca0ee6dbf794fc9e88252d18deef7d165c1138bf
giusepped <giusdesan@gmail.com>
2016-11-25 14:49:25 +0000

    corrected predicate in party members query

--- app/models/party_query_object.rb
+++ app/models/party_query_object.rb
@@ -55,7 +55,7 @@ class PartyQueryObject
                   ?partyMembership a parl:PartyMembership ;
                            parl:partyMembershipStartDate ?partyMembershipStartDate ;
                            parl:partyMembershipEndDate ?partyMembershipEndDate ;
-                           parl:partyMembershipHasParty ?member .
+                           parl:partyMembershipHasPerson ?member .
                 }
 
                 WHERE {
