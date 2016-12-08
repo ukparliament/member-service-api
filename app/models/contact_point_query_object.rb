be0ed4827a905681d5fe612ad8153adcee3e8aaa
giusepped <giusdesan@gmail.com>
2016-12-08 12:40:41 +0000

    edited contact_point_find endpoint to include name

--- app/models/contact_point_query_object.rb
+++ app/models/contact_point_query_object.rb
@@ -28,25 +28,34 @@ class ContactPointQueryObject
 
   def self.find(id)
     self.query("
-                  PREFIX parl: <http://id.ukpds.org/schema/>
-                  CONSTRUCT {
-                     ?contactPoint parl:email ?email ;
-                                    parl:telephone ?telephone ;
-                                    parl:faxNumber ?faxNumber ;
-                                    parl:streetAddress ?streetAddress ;
-                                    parl:addressLocality ?addressLocality ;
-                                    parl:postalCode ?postalCode .
-                  }
-
-                  WHERE {
-                      ?contactPoint a parl:ContactPoint ;
-                        OPTIONAL{ ?contactPoint parl:email ?email . }
-                        OPTIONAL{ ?contactPoint parl:telephone ?telephone . }
-                        OPTIONAL{ ?contactPoint parl:faxNumber ?faxNumber . }
-                        OPTIONAL{ ?contactPoint parl:streetAddress ?streetAddress . }
-                        OPTIONAL{ ?contactPoint parl:addressLocality ?addressLocality . }
-                        OPTIONAL{ ?contactPoint parl:postalCode ?postalCode . }
-                        FILTER (?contactPoint = <#{DATA_URI_PREFIX}/#{id}>)
+                PREFIX parl: <http://id.ukpds.org/schema/>
+                CONSTRUCT {
+                   ?contactPoint parl:email ?email ;
+                                 parl:telephone ?telephone ;
+                                 parl:faxNumber ?faxNumber ;
+                                 parl:streetAddress ?streetAddress ;
+                                 parl:addressLocality ?addressLocality ;
+                                 parl:postalCode ?postalCode ;
+    						                 parl:owner ?name .
+                }
+                WHERE {
+                    ?contactPoint a parl:ContactPoint ;
+                      OPTIONAL{ ?contactPoint parl:email ?email . }
+                      OPTIONAL{ ?contactPoint parl:telephone ?telephone . }
+                      OPTIONAL{ ?contactPoint parl:faxNumber ?faxNumber . }
+                      OPTIONAL{ ?contactPoint parl:streetAddress ?streetAddress . }
+                      OPTIONAL{ ?contactPoint parl:addressLocality ?addressLocality . }
+                      OPTIONAL{ ?contactPoint parl:postalCode ?postalCode ;
+                                              parl:contactPointHasSitting ?sitting .
+        				                ?sitting parl:sittingHasPerson ?person .
+    					                  ?person parl:forename ?forename ;
+    					  		                    parl:surname ?surname .
+    					                  BIND(CONCAT(?forename, ' ', ?surname) AS ?name)
+   					          }
+   						        OPTIONAL{ ?contactPoint parl:contactPointHasConstituencyParty ?cparty .
+    					 		              ?cparty parl:constituencyPartyHasConstituency ?constituency .
+    							              ?constituency parl:constituencyName ?name . }
+                    FILTER (?contactPoint = <#{DATA_URI_PREFIX}/#{id}>)
                   }
                 ")
   end
