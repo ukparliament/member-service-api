c5f1951b2928b1a1daab324015a21e0a630da2d6
giusepped <giusdesan@gmail.com>
2016-12-14 13:11:46 +0000

    added const, house, party to current members queries

--- app/models/member_query_object.rb
+++ app/models/member_query_object.rb
@@ -42,13 +42,29 @@ class MemberQueryObject
       PREFIX parl: <http://id.ukpds.org/schema/>
       CONSTRUCT{
         ?member
-        parl:forename ?forename ;
-        parl:surname ?surname .
+        	parl:forename ?forename ;
+        	parl:surname ?surname .
+    	?house a parl:House ;
+        	parl:connect ?member .
+    	?constituency a parl:Constituency ;
+        	parl:constituencyName ?constituencyName;
+         	parl:connect ?member .
+    	?party a parl:Party ;
+        	parl:partyName ?partyName;
+        	parl:connect ?member .
       }
       WHERE {
         ?sitting a parl:Sitting .
         MINUS { ?sitting a parl:PastSitting .}
         ?sitting parl:sittingHasPerson ?member .
+    	?sitting parl:sittingHasSeat ?seat .
+    	?seat parl:seatHasHouse ?house ;
+    	parl:seatHasConstituency ?constituency .
+    	OPTIONAL { ?constituency parl:constituencyName ?constituencyName . }
+    	?member parl:personHasPartyMembership ?partyMembership .
+    	  ?partyMembership parl:partyMembershipHasParty ?party .
+    		FILTER NOT EXISTS { ?partyMembership a parl:PastThing . }
+        	OPTIONAL { ?party parl:partyName ?partyName . }
         OPTIONAL { ?member parl:forename ?forename . }
         OPTIONAL { ?member parl:surname ?surname . }
       }
@@ -60,15 +76,32 @@ class MemberQueryObject
       PREFIX parl: <http://id.ukpds.org/schema/>
       CONSTRUCT{
         ?member
-            parl:forename ?forename ;
-            parl:surname ?surname .
+        	parl:forename ?forename ;
+        	parl:surname ?surname .
+    	?house a parl:House ;
+        	parl:connect ?member .
+    	?constituency a parl:Constituency ;
+        	parl:constituencyName ?constituencyName;
+         	parl:connect ?member .
+    	?party a parl:Party ;
+        	parl:partyName ?partyName;
+        	parl:connect ?member .
       }
       WHERE {
         ?sitting a parl:Sitting .
         MINUS { ?sitting a parl:PastSitting .}
         ?sitting parl:sittingHasPerson ?member .
+    	?sitting parl:sittingHasSeat ?seat .
+    	?seat parl:seatHasHouse ?house ;
+    	parl:seatHasConstituency ?constituency .
+    	OPTIONAL { ?constituency parl:constituencyName ?constituencyName . }
+    	?member parl:personHasPartyMembership ?partyMembership .
+    	  ?partyMembership parl:partyMembershipHasParty ?party .
+    		FILTER NOT EXISTS { ?partyMembership a parl:PastThing . }
+        	OPTIONAL { ?party parl:partyName ?partyName . }
         OPTIONAL { ?member parl:forename ?forename . }
         OPTIONAL { ?member parl:surname ?surname . }
+      }
     	  FILTER regex(str(?surname), \"^#{letter.upcase}\") .
       }
     ")
