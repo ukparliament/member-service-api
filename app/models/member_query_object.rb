fe75985c676bb04a9b37fb3a2c6589a4f73b85ca
Rebecca Appleyard <rklappleyard@gmail.com>
2017-01-09 11:41:52 +0000

    Updated query for all members

--- app/models/member_query_object.rb
+++ app/models/member_query_object.rb
@@ -4,17 +4,32 @@ class MemberQueryObject
   def self.all
     self.uri_builder('
       PREFIX parl: <http://id.ukpds.org/schema/>
-
-      CONSTRUCT {
-         ?member
-           parl:forename ?forename ;
-           parl:surname ?surname .
+      CONSTRUCT{
+        ?member
+          a parl:Person ;
+        	parl:forename ?forename ;
+        	parl:surname ?surname .
+    	  ?house a parl:House ;
+        	parl:connect ?member .
+    	  ?constituency a parl:Constituency ;
+        	parl:constituencyName ?constituencyName;
+         	parl:connect ?member .
+    	  ?party a parl:Party ;
+        	parl:partyName ?partyName;
+        	parl:connect ?member .
       }
       WHERE {
-        ?member
-          a parl:Member .
-            OPTIONAL { ?member parl:forename ?forename } .
-            OPTIONAL { ?member parl:surname ?surname } .
+        ?sitting a parl:Sitting .
+        ?sitting parl:sittingHasPerson ?member .
+    	  ?sitting parl:sittingHasSeat ?seat .
+    	  ?seat parl:seatHasHouse ?house ;
+    		      parl:seatHasConstituency ?constituency .
+    	  OPTIONAL { ?constituency parl:constituencyName ?constituencyName . }
+    	  ?member parl:personHasPartyMembership ?partyMembership .
+    	  ?partyMembership parl:partyMembershipHasParty ?party .
+        OPTIONAL { ?party parl:partyName ?partyName . }
+        OPTIONAL { ?member parl:forename ?forename . }
+        OPTIONAL { ?member parl:surname ?surname . }
       }
     ')
   end
