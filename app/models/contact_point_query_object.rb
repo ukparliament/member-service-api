d24bedd3a5439e10ebe50d7b23992f26a92cb7f5
Rebecca Appleyard <rklappleyard@gmail.com>
2017-01-13 10:41:48 +0000

    Changed contact point queries to comply with eager loading

--- app/models/contact_point_query_object.rb
+++ app/models/contact_point_query_object.rb
@@ -5,23 +5,25 @@ class ContactPointQueryObject
     self.uri_builder('
               PREFIX parl: <http://id.ukpds.org/schema/>
               CONSTRUCT {
-                 ?contactPoint parl:email ?email ;
-                                parl:telephone ?telephone ;
-                                parl:faxNumber ?faxNumber ;
-                                parl:streetAddress ?streetAddress ;
-                                parl:addressLocality ?addressLocality ;
-                                parl:postalCode ?postalCode .
+                 ?contactPoint
+                    a parl:ContactPoint ;
+                    parl:email ?email ;
+                    parl:telephone ?telephone ;
+                    parl:faxNumber ?faxNumber ;
+                    parl:streetAddress ?streetAddress ;
+                    parl:addressLocality ?addressLocality ;
+                    parl:postalCode ?postalCode .
               }
 
               WHERE {
                   ?contactPoint a parl:ContactPoint ;
-                      MINUS { ?contactPoint a parl:Postal}
-                    OPTIONAL{ ?contactPoint parl:email ?email . }
-                    OPTIONAL{ ?contactPoint parl:telephone ?telephone . }
-                    OPTIONAL{ ?contactPoint parl:faxNumber ?faxNumber . }
-                    OPTIONAL{ ?contactPoint parl:streetAddress ?streetAddress . }
-                    OPTIONAL{ ?contactPoint parl:addressLocality ?addressLocality . }
-                    OPTIONAL{ ?contactPoint parl:postalCode ?postalCode . }
+                  MINUS { ?contactPoint a parl:Postal}
+                  OPTIONAL{ ?contactPoint parl:email ?email . }
+                  OPTIONAL{ ?contactPoint parl:telephone ?telephone . }
+                  OPTIONAL{ ?contactPoint parl:faxNumber ?faxNumber . }
+                  OPTIONAL{ ?contactPoint parl:streetAddress ?streetAddress . }
+                  OPTIONAL{ ?contactPoint parl:addressLocality ?addressLocality . }
+                  OPTIONAL{ ?contactPoint parl:postalCode ?postalCode . }
               }'
     )
   end
@@ -30,13 +32,15 @@ class ContactPointQueryObject
     self.uri_builder("
                 PREFIX parl: <http://id.ukpds.org/schema/>
                 CONSTRUCT {
-                   ?contactPoint parl:email ?email ;
-                                 parl:telephone ?telephone ;
-                                 parl:faxNumber ?faxNumber ;
-                                 parl:streetAddress ?streetAddress ;
-                                 parl:addressLocality ?addressLocality ;
-                                 parl:postalCode ?postalCode ;
-    						                 parl:owner ?name .
+                   ?contactPoint
+                      a parl:ContactPoint ;
+                      parl:email ?email ;
+                      parl:telephone ?telephone ;
+                      parl:faxNumber ?faxNumber ;
+                      parl:streetAddress ?streetAddress ;
+                      parl:addressLocality ?addressLocality ;
+                      parl:postalCode ?postalCode ;
+    						      parl:owner ?name .
                 }
                 WHERE {
                     ?contactPoint a parl:ContactPoint ;
@@ -60,5 +64,4 @@ class ContactPointQueryObject
                   }
                 ")
   end
-
 end
