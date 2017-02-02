52cfba1f6671fea6a1a8caf187c83533b10fc80b
Rebecca Appleyard <rklappleyard@gmail.com>
2017-02-02 12:32:09 +0000

    Completed refactor of person find query

--- app/models/person_query_object.rb
+++ app/models/person_query_object.rb
@@ -6,13 +6,14 @@ class PersonQueryObject
       PREFIX parl: <http://id.ukpds.org/schema/>
       CONSTRUCT {
         ?person
-          parl:forename ?forename ;
-          parl:surname ?surname .
+        	a parl:Person ;
+         	parl:personGivenName ?givenName ;
+         	parl:personFamilyName ?familyName .
       }
       WHERE {
         ?person a parl:Person .
-        OPTIONAL { ?person parl:forename ?forename } .
-        OPTIONAL { ?person parl:surname ?surname } .
+        OPTIONAL { ?person parl:personGivenName ?givenName } .
+        OPTIONAL { ?person parl:personFamilyName ?familyName } .
       }'
     )
   end
@@ -22,15 +23,16 @@ class PersonQueryObject
       PREFIX parl: <http://id.ukpds.org/schema/>
       CONSTRUCT {
         ?person
-          parl:forename ?forename ;
-          parl:surname ?surname ;
+        	a parl:Person ;
+         	parl:personGivenName ?givenName ;
+         	parl:personFamilyName ?familyName .
       }
       WHERE {
         ?person a parl:Person .
-        OPTIONAL { ?person parl:forename ?forename } .
-        OPTIONAL { ?person parl:surname ?surname } .
+        OPTIONAL { ?person parl:personGivenName ?givenName } .
+        OPTIONAL { ?person parl:personFamilyName ?familyName } .
 
-    	  FILTER regex(str(?surname), \"^#{letter.upcase}\") .
+    	  FILTER regex(str(?familyName), \"^#{letter.upcase}\") .
       }
     ")
   end
@@ -41,80 +43,104 @@ class PersonQueryObject
       PREFIX schema: <http://schema.org/>
       PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
       CONSTRUCT {
-          <#{DATA_URI_PREFIX}/#{id}>
+          ?person
               a parl:Person ;
-              parl:dateOfBirth ?dateOfBirth ;
-              parl:forename ?forename ;
-              parl:middleName ?middleName ;
-              parl:surname ?surname ;
-              parl:gender ?gender .
+              parl:personDateOfBirth ?dateOfBirth ;
+              parl:personGivenName ?givenName ;
+              parl:personOtherName ?otherName ;
+              parl:personFamilyName ?familyName ;
+    		      parl:personHasGenderIdentiy ?genderIdentity .
+    		?genderIdentity
+        		a parl:GenderIdentity ;
+        		parl:genderIdentityHasGender ?gender .
+    		?gender
+        		a parl:Gender ;
+        		parl:genderName ?genderName .
      	  ?contactPoint
             a parl:ContactPoint ;
         	  parl:email ?email ;
-        	  parl:telephone ?telephone ;
+        	  parl:phoneNumber ?phoneNumber ;
         	  parl:faxNumber ?faxNumber ;
-        	  parl:streetAddress ?streetAddress ;
-        	  parl:addressLocality ?addressLocality ;
-        	  parl:postalCode ?postalCode .
+    		    parl:contactPointHasPostalAddress ?postalAddress .
+    	  ?postalAddress
+        	  a parl:PostalAddress ;
+        	  parl:addressLine1 ?addressLine1 ;
+			      parl:addressLine2 ?addressLine2 ;
+        	  parl:addressLine3 ?addressLine3 ;
+        	  parl:addressLine4 ?addressLine4 ;
+        	  parl:addressLine5 ?addressLine5 ;
+        	  parl:postCode ?postCode .
     	  ?constituency
-        	  a parl:Constituency ;
-             parl:constituencyName ?constituencyName ;
-        	  parl:constituencyStartDate ?constituencyStartDate ;
-        	  parl:constituencyEndDate ?constituencyEndDate .
-    	  ?sitting
-            a parl:Sitting ;
-        	  parl:sittingEndDate ?sittingEndDate ;
-        	  parl:sittingStartDate ?sittingStartDate ;
-       		  parl:connect ?constituency ;
-        	  parl:connect ?house ;
-            parl:relationship \"through\" .
-    	?party
-        	  a parl:Party ;
-             parl:partyName ?partyName .
-    	?partyMembership
-            a parl:PartyMembership ;
-        	  parl:partyMembershipStartDate ?partyMembershipStartDate ;
-        	  parl:partyMembershipEndDate ?partyMembershipEndDate ;
-       		  parl:connect ?party ;
-            parl:relationship \"through\" .
-    	?house a parl:House .
+        	   a parl:ConstituencyGroup ;
+             parl:constituencyGroupName ?constituencyName .
+    	  ?seatIncumbency
+          		a parl:SeatIncumbency ;
+        	  	parl:seatIncumbencyEndDate ?seatIncumbencyEndDate ;
+        	  	parl:seatIncumbencyStartDate ?seatIncumbencyStartDate ;
+				      parl:seatIncumbencyHasHouseSeat ?seat .
+        ?seat
+            	a parl:HouseSeat ;
+            	parl:houseSeatHasConstituencyGroup ?constituency ;
+            	parl:houseSeatHasHouse ?house .
+    		?party
+        	  	a parl:Party ;
+             	parl:partyName ?partyName .
+    		?partyMembership
+            	a parl:PartyMembership ;
+        	  	parl:partyMembershipStartDate ?partyMembershipStartDate ;
+        	  	parl:partyMembershipEndDate ?partyMembershipEndDate ;
+				      parl:partyMembershipHasParty ?party .
+    		?house a parl:House ;
+            	parl:houseName ?houseName .
       }
       WHERE {
-        <#{DATA_URI_PREFIX}/#{id}> a parl:Person .
-        OPTIONAL { <#{DATA_URI_PREFIX}/#{id}> parl:forename ?forename } .
-        OPTIONAL { <#{DATA_URI_PREFIX}/#{id}> parl:middleName ?middleName } .
-        OPTIONAL { <#{DATA_URI_PREFIX}/#{id}> parl:surname ?surname } .
-        OPTIONAL { <#{DATA_URI_PREFIX}/#{id}> parl:dateOfBirth ?dateOfBirth } .
-        ?gender rdfs:subClassOf parl:HasGender .
-        <#{DATA_URI_PREFIX}/#{id}> a ?gender .
-        FILTER NOT EXISTS { ?gender rdfs:seeAlso schema:GenderType } .
+        ?person a parl:Person .
+        OPTIONAL { ?person parl:personGivenName ?givenName } .
+        OPTIONAL { ?person parl:personOtherName ?otherName } .
+        OPTIONAL { ?person parl:personFamilyName ?familyName } .
+        OPTIONAL { ?person parl:personDateOfBirth ?dateOfBirth } .
+        ?person parl:personHasGenderIdentity ?genderIdentity .
+        ?genderIdentity parl:genderIdentityHasGender ?gender .
+        OPTIONAL { ?gender parl:genderName ?genderName . }
 
-    	<#{DATA_URI_PREFIX}/#{id}> parl:personHasSitting ?sitting .
-    	?sitting parl:sittingHasSeat ?seat .
-    	?seat parl:seatHasConstituency ?constituency .
-        ?seat parl:seatHasHouse ?house .
-        OPTIONAL { ?sitting parl:endDate ?sittingEndDate . }
-        OPTIONAL { ?sitting parl:startDate ?sittingStartDate . }
-        OPTIONAL { ?constituency parl:constituencyName ?constituencyName . }
-        OPTIONAL { ?constituency parl:constituencyStartDate ?constituencyStartDate . }
-		OPTIONAL { ?constituency parl:constituencyEndDate ?constituencyEndDate . }
+    	  OPTIONAL {
+    	      ?person parl:memberHasSeatIncumbency ?seatIncumbency .
+    	  	  ?seatIncumbency parl:seatIncumbencyHasHouseSeat ?seat .
+    	  	  ?seat parl:houseSeatHasConstituencyGroup ?constituency .
+    	      ?seat parl:houseSeatHasHouse ?house .
+    	      OPTIONAL { ?seatIncumbency parl:seatIncumbencyEndDate ?seatIncumbencyEndDate . }
+    	      ?seatIncumbency parl:seatIncumbencyStartDate ?seatIncumbencyStartDate .
+    	      ?constituency parl:constituencyGroupName ?constituencyName .
+    	      ?house parl:houseName ?houseName .
+    	  }
 
-    	<#{DATA_URI_PREFIX}/#{id}> parl:personHasPartyMembership ?partyMembership .
-        ?partyMembership parl:partyMembershipHasParty ?party .
-        OPTIONAL { ?partyMembership parl:partyMembershipStartDate ?partyMembershipStartDate . }
-        OPTIONAL { ?partyMembership parl:partyMembershipEndDate ?partyMembershipEndDate . }
-        OPTIONAL { ?party parl:partyName ?partyName . }
+        OPTIONAL {
+    	    ?person parl:partyMemberHasPartyMembership ?partyMembership .
+          ?partyMembership parl:partyMembershipHasParty ?party .
+          ?partyMembership parl:partyMembershipStartDate ?partyMembershipStartDate .
+          OPTIONAL { ?partyMembership parl:partyMembershipEndDate ?partyMembershipEndDate . }
+          ?party parl:partyName ?partyName .
+        }
 
-    	OPTIONAL {
-        	?sitting parl:sittingHasContactPoint ?contactPoint .
-        	OPTIONAL{ ?contactPoint parl:email ?email . }
-        	OPTIONAL{ ?contactPoint parl:telephone ?telephone . }
-        	OPTIONAL{ ?contactPoint parl:faxNumber ?faxNumber . }
-        	OPTIONAL{ ?contactPoint parl:streetAddress ?streetAddress . }
-        	OPTIONAL{ ?contactPoint parl:addressLocality ?addressLocality . }
-        	OPTIONAL{ ?contactPoint parl:postalCode ?postalCode . }
-    	}
-      }")
+    	  OPTIONAL { ?person parl:personHasContactPoint ?contactPoint .
+        	OPTIONAL { ?contactPoint parl:phoneNumber ?phoneNumber . }
+        	OPTIONAL { ?contactPoint parl:email ?email . }
+        	OPTIONAL { ?contactPoint parl:faxNumber ?faxNumber . }
+
+        	OPTIONAL {
+        	    ?contactPoint parl:contactPointHasPostalAddress ?postalAddress .
+				      OPTIONAL { ?postalAddress parl:addressLine1 ?addressLine1 . }
+				      OPTIONAL { ?postalAddress parl:addressLine2 ?addressLine2 . }
+        		  OPTIONAL { ?postalAddress parl:addressLine3 ?addressLine3 . }
+        		  OPTIONAL { ?postalAddress parl:addressLine4 ?addressLine4 . }
+        		  OPTIONAL { ?postalAddress parl:addressLine5 ?addressLine5 . }
+        		  OPTIONAL { ?postalAddress parl:postCode ?postCode . }
+        	}
+    	  }
+
+        FILTER(?person=<#{DATA_URI_PREFIX}/#{id}>)
+      }
+    ")
   end
 
   def self.constituencies(id)
