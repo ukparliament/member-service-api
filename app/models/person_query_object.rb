6fec3c11e98b1cca457602e7a9dffbf758c2dc81
Rebecca Appleyard <rklappleyard@gmail.com>
2017-03-02 11:44:59 +0000

    Small change to person find query

--- app/models/person_query_object.rb
+++ app/models/person_query_object.rb
@@ -71,62 +71,50 @@ class PersonQueryObject
 
   def self.find(id)
     self.uri_builder("
-      PREFIX parl: <http://id.ukpds.org/schema/>
-      PREFIX schema: <http://schema.org/>
+     PREFIX parl: <http://id.ukpds.org/schema/>
       PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
       CONSTRUCT {
-          ?person
-              a parl:Person ;
+          ?person a parl:Person ;
               parl:personDateOfBirth ?dateOfBirth ;
               parl:personGivenName ?givenName ;
               parl:personOtherNames ?otherName ;
               parl:personFamilyName ?familyName ;
     		      parl:personHasGenderIdentity ?genderIdentity ;
               parl:partyMemberHasPartyMembership ?partyMembership .
-    		?genderIdentity
-        		a parl:GenderIdentity ;
+    		?genderIdentity a parl:GenderIdentity ;
         		parl:genderIdentityHasGender ?gender .
-    		?gender
-        		a parl:Gender ;
+    		?gender a parl:Gender ;
         		parl:genderName ?genderName .
-     	  ?contactPoint
-            a parl:ContactPoint ;
+     	  ?contactPoint a parl:ContactPoint ;
         	  parl:email ?email ;
         	  parl:phoneNumber ?phoneNumber ;
         	  parl:faxNumber ?faxNumber ;
     		    parl:contactPointHasPostalAddress ?postalAddress .
-    	  ?postalAddress
-        	  a parl:PostalAddress ;
+    	  ?postalAddress a parl:PostalAddress ;
         	  parl:addressLine1 ?addressLine1 ;
 			      parl:addressLine2 ?addressLine2 ;
         	  parl:addressLine3 ?addressLine3 ;
         	  parl:addressLine4 ?addressLine4 ;
         	  parl:addressLine5 ?addressLine5 ;
         	  parl:postCode ?postCode .
-    	  ?constituency
-        	   a parl:ConstituencyGroup ;
+    	  ?constituency a parl:ConstituencyGroup ;
              parl:constituencyGroupName ?constituencyName .
-    	  ?seatIncumbency
-          		a parl:SeatIncumbency ;
+    	  ?seatIncumbency a parl:SeatIncumbency ;
         	  	parl:incumbencyEndDate ?incumbencyEndDate ;
         	  	parl:incumbencyStartDate ?incumbencyStartDate ;
 				      parl:seatIncumbencyHasHouseSeat ?seat ;
         		  parl:incumbencyHasContactPoint ?contactPoint .
-    	  ?houseIncumbency
-        		a parl:HouseIncumbency ;
+    	?houseIncumbency a parl:HouseIncumbency ;
         		parl:incumbencyEndDate ?incumbencyEndDate ;
         	  parl:incumbencyStartDate ?incumbencyStartDate ;
         		parl:houseIncumbencyHasHouse ?house ;
         		parl:incumbencyHasContactPoint ?contactPoint .
-        ?seat
-            	a parl:HouseSeat ;
+        ?seat a parl:HouseSeat ;
             	parl:houseSeatHasConstituencyGroup ?constituency ;
             	parl:houseSeatHasHouse ?house .
-    		?party
-        	  	a parl:Party ;
+    		?party a parl:Party ;
              	parl:partyName ?partyName .
-    		?partyMembership
-            	a parl:PartyMembership ;
+    		?partyMembership a parl:PartyMembership ;
         	  	parl:partyMembershipStartDate ?partyMembershipStartDate ;
         	  	parl:partyMembershipEndDate ?partyMembershipEndDate ;
 				      parl:partyMembershipHasParty ?party .
@@ -135,43 +123,32 @@ class PersonQueryObject
       }
       WHERE {
         BIND(<#{DATA_URI_PREFIX}/#{id}> AS ?person)
-
         OPTIONAL { ?person parl:personGivenName ?givenName } .
         OPTIONAL { ?person parl:personOtherNames ?otherName } .
         OPTIONAL { ?person parl:personFamilyName ?familyName } .
-        OPTIONAL { ?person parl:personDateOfBirth ?dateOfBirth } .
-        ?person parl:personHasGenderIdentity ?genderIdentity .
-        ?genderIdentity parl:genderIdentityHasGender ?gender .
-        OPTIONAL { ?gender parl:genderName ?genderName . }
-
     	  OPTIONAL {
     	      ?person parl:memberHasIncumbency ?incumbency .
             OPTIONAL { ?incumbency parl:incumbencyEndDate ?incumbencyEndDate . }
     	      ?incumbency parl:incumbencyStartDate ?incumbencyStartDate .
-
         	  {
         	      ?incumbency a parl:HouseIncumbency .
-                  BIND(?incumbency AS ?houseIncumbency)
-                  ?houseIncumbency parl:houseIncumbencyHasHouse ?house .
+                BIND(?incumbency AS ?houseIncumbency )
+                ?houseIncumbency parl:houseIncumbencyHasHouse ?house .
             	  ?house parl:houseName ?houseName .
-        	  }
-
-        	  UNION {
+        	  } UNION {
         	      ?incumbency a parl:SeatIncumbency .
-                BIND(?incumbency AS ?seatIncumbency)
+                BIND(?incumbency AS ?seatIncumbency )
                 ?seatIncumbency parl:seatIncumbencyHasHouseSeat ?seat .
             	  ?seat parl:houseSeatHasConstituencyGroup ?constituency .
     	      	  ?seat parl:houseSeatHasHouse ?house .
             	  ?house parl:houseName ?houseName .
             	  ?constituency parl:constituencyGroupName ?constituencyName .
         	  }
-
             OPTIONAL {
         	    ?incumbency parl:incumbencyHasContactPoint ?contactPoint .
         	    OPTIONAL { ?contactPoint parl:phoneNumber ?phoneNumber . }
         	    OPTIONAL { ?contactPoint parl:email ?email . }
         	    OPTIONAL { ?contactPoint parl:faxNumber ?faxNumber . }
-
         	    OPTIONAL {
         	        ?contactPoint parl:contactPointHasPostalAddress ?postalAddress .
 				          OPTIONAL { ?postalAddress parl:addressLine1 ?addressLine1 . }
@@ -182,9 +159,7 @@ class PersonQueryObject
         	    	  OPTIONAL { ?postalAddress parl:postCode ?postCode . }
         	    }
     	      }
-    	    }
-
-          OPTIONAL {
+          } OPTIONAL {
     	      ?person parl:partyMemberHasPartyMembership ?partyMembership .
             ?partyMembership parl:partyMembershipHasParty ?party .
             ?partyMembership parl:partyMembershipStartDate ?partyMembershipStartDate .
